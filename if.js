3883ebf2db3989c5b4377f7a7129a132c2a2c7da
Alejandro Silva <a.s.cebreros@gmail.com>
2021-06-09 13:02:51 -0500

    Practicando condicional if

---
+++ if.js
@@ -0,0 +1,13 @@
+var edad = 18;
+
+if(edad === 18) {
+    console.log('Puedes votar, será tu primera vez.');
+} else if(edad > 18) {
+    console.log('Puedes votar de nuevo.');
+} else {
+    console.log('Aún no puedes votar.');
+}
+
+var number = 1;
+
+var resultado = number === 1 ? 'Sí, soy un uno' : 'No, no soy uno'
