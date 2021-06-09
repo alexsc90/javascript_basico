d464315478f4a21604f3c39fcf4c84981a672e25
Alejandro Silva <a.s.cebreros@gmail.com>
2021-06-09 13:12:43 -0500

    Practicando con arrays

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var frutas = ['Manzana', 'Plátano', 'Cereza', 'Fresa'];
+
+console.log(frutas);
+
+console.log(frutas.length);
+
+console.log(frutas[0]); 
+
+frutas.push('Uvas')
+
+console.log(frutas)
+
+console.log(frutas.length) 
+
+frutas.pop()
+
+console.log(frutas)
+
+frutas.unshift('Uvas')
+
+console.log(frutas)
+
+frutas.shift()
+
+console.log(frutas)
+ 
+frutas.indexOf('Cereza')
